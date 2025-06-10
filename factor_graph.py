e9c5c40895758026ae848397974d9833a2225fce
rishibhp <r.prakash@mail.utoronto.ca>
2025-06-10 15:50:03 -0300

    Added more comments and references + cleaned up code

--- factor_graph.py
+++ factor_graph.py
@@ -6,7 +6,7 @@ from data_loader import data_load_into_graph as load
 from attack_correlation import EVENT_TYPE_TO_MITRE
 
 FALSE_INDICATION = 0.2
-MAX_ITER = 30
+MAX_ITER = 10
 
 class Node:
     """
@@ -30,7 +30,7 @@ class Node:
 class VariableNode(Node):
     """
     Class for variable nodes in factor graphs.
-    For us, these will hold the tactics in an incident
+    For us, these will hold the tactics in an incident.
     """
     def is_valid_neighbour(self, node: Node):
         return isinstance(node, FactorNode)
@@ -58,16 +58,20 @@ class FactorNode(Node):
 
 class Messages:
     """
-    Class to keep track of all the messages passing between nodes
+    Class to keep track of all the messages passing between nodes.
+    Implements the sumproduct algorithm as outlined in https://vision.unipv.it/IA2/Factor%20graphs%20and%20the%20sum-product%20algorithm.pdf
+    and section 5.1.2 of http://web4.cs.ucl.ac.uk/staff/D.Barber/textbook/180325.pdf.
     """
     def __init__(self):
-        self.messages = dict() # keeps track of the messages on an edge. Means messages don't have to be recomputed
         self.i = 0 # roughly keeps track of how many iterations have happened
         
-    def _variable_to_factor_message(self, variable: VariableNode, factor: FactorNode):
-        """ Computes variable to factor messages as outlined in  """
+    def variable_to_factor_message(self, variable: VariableNode, factor: FactorNode) -> np.ndarray:
+        """ Computes messages from variable to factor.
+        Formula given in (5) of the paper and Procedure 5.1 of Barber 
+        """
+        self.i += 1
         if self.i >= MAX_ITER:
-            return 1
+            return np.array([1., 1.])
         
         incoming_messages = [
             self.factor_to_variable_message(factor2, variable)
@@ -75,12 +79,15 @@ class Messages:
             if factor2.name != factor.name
         ]
 
-        # base case is also handled here
-        # returns 1 on empty product
         return np.prod(incoming_messages, axis=0)
-
-    def _factor_to_variable_message(self, factor: FactorNode, variable: VariableNode):
-        """ Relies on the fact that a factor depends on at most 2 variables. """
+    
+    def factor_to_variable_message(self, factor: FactorNode, variable: VariableNode) -> np.ndarray:
+        """
+        Computes factor to variable messages for sum product algorithm.
+        Formula given in (6) of paper and Procedure 5.1 of Barber
+        Relies on the fact that a factor depends on at most 2 variables.
+        """
+        self.i += 1
         factor_distr = np.copy(factor.distr)
         var_index = factor.neighbours.index(variable) # variable should be a neighbour of factor. TODO: Check this?
         
@@ -88,43 +95,23 @@ class Messages:
         if len(factor.neighbours) == 1:
             return factor_distr
         
-        assert len(factor.neighbours) == 2 # Only other case. Code below assumes this is the case
-
         if self.i >= MAX_ITER:
             return np.squeeze(np.sum(factor_distr, axis=1 - var_index))
+        
+        assert len(factor.neighbours) == 2 # Only other case. Code below assumes this is the case
         
         # pointwise product along appropriate axis
         incoming_message = self.variable_to_factor_message(factor.neighbours[1 - var_index], factor) # should be a 1x2 vector
         assert incoming_message.shape == (2,)
-        factor_distr *= np.stack((incoming_message, incoming_message), axis=var_index)
-
-        return np.squeeze(np.sum(factor_distr, axis=1 - var_index))
-
-    def variable_to_factor_message(self, variable: VariableNode, factor: FactorNode):
-        self.i += 1
-        if self.i >= MAX_ITER:
-            return np.array([1., 1.])
         
-        message_name = (variable.name, factor.name)
-        if message_name not in self.messages:
-            self.messages[message_name] = self._variable_to_factor_message(variable, factor)
-        return self.messages[message_name]
-    
-    def factor_to_variable_message(self, factor: FactorNode, variable: VariableNode):
-        self.i += 1
-        if self.i >= MAX_ITER:
-            if len(factor.neighbours) == 1:
-                return factor.distr
-            var_index = factor.neighbours.index(variable)
-            return np.squeeze(np.sum(factor.distr, axis=1 - var_index))
-        message_name = (factor.name, variable.name)
-        if message_name not in self.messages:
-            self.messages[message_name] = self._factor_to_variable_message(factor, variable)
-        return self.messages[message_name]
+        factor_distr *= np.stack((incoming_message, incoming_message), axis=var_index) # This lines multiplies the variable messages with the appropriate axis
+
+        return np.squeeze(np.sum(factor_distr, axis=1 - var_index)) # Here we sum over the variables (not including the given one)
+
     
     def marginal(self, variable: VariableNode) -> np.ndarray:
         """
-        Computes distribution for given variable.
+        Computes distribution for given variable. Also given in Procedure 5.1 of Barber
 
         Returns:
         np.ndarray: array of shape (2,) corresponding to probabilty distribution of variable being active or inactive
@@ -187,7 +174,7 @@ class FactorGraph:
                 ta_node.add_neighbour(fact_node)
                 tb_node.add_neighbour(fact_node)
 
-                mu = MITRE_TRANSITION[tactic_a][tactic_b]
+                mu = MITRE_TRANSITION[tactic_a][tactic_b] # TODO: Change mu so it takes into account temporal data
                 fact_node.set_distr(np.array([[FALSE_INDICATION, 1 - mu], [1 - mu, mu]]))
 
 
@@ -197,8 +184,4 @@ if __name__ == "__main__":
     fg = FactorGraph(alerts)
 
     m = Messages()
-    # v = fg.variables["Collection"]
-    # print(m.marginal(v))
-    # v = fg.variables["Credential Access"]
-    # print(m.marginal(v))
     print(m.marginals(fg))

8094306e413a6a9771b1f5c5ee50023f2e4b1201
rishibhp <r.prakash@mail.utoronto.ca>
2025-06-12 15:54:00 -0300

    Added modular ScoreCalculator component

--- config.py
+++ config.py
@@ -3,7 +3,8 @@ import json
 CORRELATION_THRESHOLD = 0.4
 FALSE_INDICATION = 0.2 # used by factor graphs
 DATA_FILEPATH         = "security_data_assignment.json" # This file is gitignored
-DEFAULT_SCORES_PATH = "data/scores.txt"
+DEFAULT_SCORES_PATH_TXT = "scores/scores.txt"
+DEFAULT_SCORES_PATH_JSON = "scores/scores.json"
 
 MITRE_TACTICS = [
   "Initial Access", "Execution", "Persistence", "Privilege Escalation",
